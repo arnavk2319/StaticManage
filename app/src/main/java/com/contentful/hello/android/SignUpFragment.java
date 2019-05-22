85c9d81609c12bea97279c46723f587351d2e6bf
khmoiz <khmoiz@sheridancollege.ca>
2019-05-22 15:45:09 -0400

    Finished functionality of Spinners. Where Country is selected and corresponding states/provinces are loaded into the next spinner for the user.

--- app/src/main/java/com/contentful/hello/android/SignUpFragment.java
+++ app/src/main/java/com/contentful/hello/android/SignUpFragment.java
@@ -7,29 +7,39 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
-import android.widget.CompoundButton;
 import android.widget.EditText;
 import android.widget.LinearLayout;
 import android.widget.RadioButton;
-import android.widget.RadioGroup;
 import android.widget.Spinner;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class SignUpFragment extends Fragment {
 
     TextView createAccountTextView,passwordTextView,capitalTextView,numberTextView,eightCharTextView;
     EditText fullNameEditText, nickNameEditText, emailEditText, passwordEditText, phoneNum, pinCode;
 
-    private Spinner country, province;
+    private Spinner country, states;
 
     RadioButton nickNameRadioButton;
     LinearLayout linearLayout;
+    private String[] countries, canada, america;
+
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+
+
         View view =inflater.inflate(R.layout.sign_up_fragment,container,false);
+
+
+        countries = getResources().getStringArray(R.array.country_arrays);
+        canada = getResources().getStringArray(R.array.canada);
+        america = getResources().getStringArray(R.array.america);
+
 
         createAccountTextView = view.findViewById(R.id.headingTextView);
         passwordTextView = view.findViewById(R.id.passwordText);
@@ -46,25 +56,64 @@ public class SignUpFragment extends Fragment {
         phoneNum = view.findViewById(R.id.edtTxt_phoneNumber);
         pinCode = view.findViewById(R.id.edtTxt_pinCode);
         country = view.findViewById(R.id.spin_country);
-        province = view.findViewById(R.id.spin_province);
+        states = view.findViewById(R.id.spin_province);
 
         nickNameRadioButton = view.findViewById(R.id.nicknameRadioButton);
 
         linearLayout = view.findViewById(R.id.linearLayout);
         linearLayout.setElevation((float)15.0);
 
-        updateSpinnerItems();
+        updateCountrySpinnerItems();
         
         return view;
 
     }
 
-    private void updateSpinnerItems() {
+    @Override
+    public void onStart() {
+        super.onStart();
+
+        country.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parentView, View selectedItemView, int position, long id) {
+                //In case we need the string selection from the string array values
+                //We can also use get the value by using states.getPrompt()
+                //String selection = countries[position];
+
+                if (position == 0) {
+                    states.setAdapter(null);
+                } else if (position == 1) {
+                    updateStateSpinnerItems(canada);
+                } else {
+                    updateStateSpinnerItems(america);
+                }
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parentView) {
+                //pass
+            }
+
+        });
+    }
+
+    private void updateCountrySpinnerItems() {
 
         ArrayAdapter<String> dataAdapter = new ArrayAdapter<String>(getContext(),
                 android.R.layout.simple_spinner_item, getResources().getStringArray(R.array.country_arrays));
         dataAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
 
         country.setAdapter(dataAdapter);
+    }
+
+    private void updateStateSpinnerItems(String[] states) {
+
+        this.states.setAdapter(null);
+
+        ArrayAdapter<String> dataAdapter = new ArrayAdapter<String>(getContext(),
+                android.R.layout.simple_spinner_item, states);
+        dataAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+
+        this.states.setAdapter(dataAdapter);
     }
 }
